9e4e9f25aa53e3eca52a0c2535dc6cf842131b27
donetian-petkov <donetianpetkov@gmail.com>
2023-08-14 19:51:32 +0300

    run_gitea.sh changes

--- run_gitea.sh
+++ run_gitea.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+  apt-get update
+  apt-get install -y git
 
-  mkdir source_repo || true
-  cd source_repo || exit
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+  git config --global --add safe.directory /var/www/html
 
-  git clone http://192.168.111.202:3000/myadminuser/supercalc 
+  git init
+  git checkout -b main
+  git add .
+  git commit -m "Initial commit"
 
-  git commit -m "Initial Commit"
+  git remote add origin http://192.168.111.202:3000/donetian/supercalc.git
   git push -u origin main
